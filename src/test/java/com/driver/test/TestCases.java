9d26625520c08c389172d306bd55c55e30025f33
somalingck <136596008+somalingck@users.noreply.github.com>
2023-09-04 13:11:18 +0530

    Initial commit

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,15 +1,9 @@
 package com.driver.test;
 
 import com.driver.AirBnb;
-import com.driver.controllers.HotelManagementController;
-import com.driver.model.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.*;
 
 @SpringBootTest(classes = AirBnb.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
